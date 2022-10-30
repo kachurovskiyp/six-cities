ed9fdfb4acc39ae7808bef32f390e17c9c11f482
Pavlo <kachurovskiy.p@gmail.com>
2022-10-30 18:51:34 +0100

    Маршрут по тайным тропам

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,11 +1,48 @@
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import { AppRoute, AutorizationStatus } from '../../const';
+import PrivateRoute from '../private-route/private-route';
+
+import Login from '../../pages/login/login';
+import Favorites from '../../pages/favorites/favorites';
+import Property from '../../pages/property/property';
 import Main from '../../pages/main/main';
+import NotFound from '../../pages/not-found/not-found';
 
 type MainProps = {
   offerCount: number;
 }
 
 function App({offerCount}: MainProps): JSX.Element {
-  return <Main offerCount={offerCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Main}
+          element={<Main offerCount={offerCount}/>}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<Login/>}
+        />
+        <Route
+          path={AppRoute.Property}
+          element={<Property/>}
+        />
+        <Route
+          path={AppRoute.Favorites}
+          element={
+            <PrivateRoute authorizationStatus={AutorizationStatus.NoAuth}>
+              <Favorites/>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="*"
+          element={<NotFound/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ project/src/const.ts
@@ -0,0 +1,12 @@
+export enum AppRoute {
+  Login = '/login',
+  Favorites = '/favorites',
+  Property = '/property',
+  Main = '/'
+}
+
+export enum AutorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Uknown = 'UNKNOWN'
+}
