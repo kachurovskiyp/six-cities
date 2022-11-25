218a7ab335a282233db80fb0e37eeacfd52e6d13
Pavlo <kachurovskiy.p@gmail.com>
2022-11-25 18:59:19 +0100

    Больше подробностей (часть 2)

---
+++ project/src/mocks/reviews.ts
@@ -0,0 +1,23 @@
+import { Reviews } from '../types/reviews-type';
+
+export const reviews: Reviews = [
+  {
+    avatar: 'avatar-max.jpg',
+    name: 'Max',
+    rating: 80,
+    review: 'A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.'
+  },
+  {
+    avatar: 'avatar-max.jpg',
+    name: 'Smith',
+    rating: 75,
+    review: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s'
+  },
+  {
+    avatar: 'avatar-max.jpg',
+    name: 'Marta',
+    rating: 90,
+    review: 'It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+  }
+];
+
